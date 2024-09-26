17525bf1914cb13b1a6470fa4a70d8fd1d41e552
Variniia Zavadskaia <variniyaal@gmail.com>
2024-09-26 19:32:44 +0300

    css fix

--- cmps/AddBook.jsx
+++ cmps/AddBook.jsx
@@ -3,52 +3,10 @@ const { useState } = React
 import { bookService } from '../services/book.service.js'
 
 export function BookAdd({ onAddBook }) {
-    const [bookToAdd, setBookToAdd] = useState([
-        { id: 1, title: 'To Kill a Mockingbird' },
-        { id: 2, title: '1984' },
-        { id: 3, title: 'The Great Gatsby' },
-        { id: 4, title: 'The Catcher in the Rye' },
-        { id: 5, title: 'Moby-Dick' },
-      ])
+    const [bookToAdd, setBookToAdd] = useState()
 
-    // function handleChange({ target }) {
-    //     const field = target.name
-    //     console.log(target.name)
-
-    //     let value = target.value
-
-    //     switch (target.type) {
-    //         case 'number':
-    //         case 'range':
-    //             value = +value
-    //             break
-
-    //         case 'checkbox':
-    //             value = target.checked
-    //             break
-    //     }
-    //     console.log(value)
-
-    //     if (field === 'listPrice') {
-    //         setBookToAdd(prevBook => ({
-    //             ...prevBook,
-    //             listPrice: { ...prevBook.listPrice, amount: value },
-    //         }))
-    //     } else {
-    //         setBookToAdd(prevBook => ({ ...prevBook, [field]: value }))
-    //     }
-    // }
-
-    // function onSubmit(ev) {
-    //     ev.preventDefault()
-    //     onAddBook(bookToAdd)
-    //     setBookToAdd({ title: '', listPrice: { amount: 0 } })
-    // }
-
-    const addBook = (book) => {
-        console.log(`Adding book: ${book.title}`); // Replace with your add logic
-        // Here you can call bookService.addGoogleBook(book) or similar
-      };
+    
+  
 
     const { title, listPrice } = bookToAdd
     const isValid = title && listPrice.amount
